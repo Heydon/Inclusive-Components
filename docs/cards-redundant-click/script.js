dee1153061744999968438ab4612d5167261b0d5
Heydon Pickering <heydon@heydonworks.com>
2018-06-05 17:26:01 +0100

    cursor via JS

--- docs/cards-redundant-click/script.js
+++ docs/cards-redundant-click/script.js
@@ -1,6 +1,7 @@
 const cards = document.querySelectorAll('.card');
 Array.prototype.forEach.call(cards, card => {
     let down, up, link = card.querySelector('h2 a');
+    card.style.cursor = 'pointer';
     card.onmousedown = () => down = +new Date();
     card.onmouseup = () => {
         up = +new Date();
