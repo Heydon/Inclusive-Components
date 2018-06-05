c471413bdc3f6079826bfdd5af293828ccf6b00b
Heydon Pickering <heydon@heydonworks.com>
2018-06-05 08:50:27 +0100

    mouse timings

--- docs/cards-redundant-click-read-more/script.js
+++ docs/cards-redundant-click-read-more/script.js
@@ -1,5 +1,11 @@
 const cards = document.querySelectorAll('.card');
 Array.prototype.forEach.call(cards, card => {
-    let link = card.querySelector('h2 a');
-    card.addEventListener('click', () => link.click());
+    let down, up, link = card.querySelector('h2 a');
+    card.onmousedown = () => down = +new Date();
+    card.onmouseup = () => {
+        up = +new Date();
+        if ((up - down) < 200) {
+            link.click();
+        }
+    }
 });
